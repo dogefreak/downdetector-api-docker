78a9cdf965dc0ba7ff4f7a3453af6a3e94497811
Dogefreak <60531982+dogefreak@users.noreply.github.com>
2024-04-08 10:51:50 +0200

    Create tester.js

---
+++ test/tester.js
@@ -0,0 +1,17 @@
+const { downdetector } = require('downdetector-api');
+
+async function main () {
+  try {
+    // Without specifying the downdetector domain
+    // const response = await downdetector('steam');
+    // Specifying the downdetector domain (some companies are not in the .com domain)
+    const response = await downdetector('ziggo', 'nl');
+    console.log("Response: " + response);
+    console.log('Data: ' + JSON.stringify(response));
+    // console.log("Hello!")
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
