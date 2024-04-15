9a130c4b507b3e1521bf40067073aa804a60657a
Dogefreak <60531982+dogefreak@users.noreply.github.com>
2024-04-15 13:38:00 +0200

    Update puWorker.js

--- puWorker.js
+++ puWorker.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 // WARNING: don't use console.log here for debug, use console.error instead. STDOUT is used to deliver output data
 
 // Get command line arguments for company and domain
